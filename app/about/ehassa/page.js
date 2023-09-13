15dce56508261ddd868527b31c93b60580101acd
owusuvictorjnr <owusuvictor80@gmail.com>
2023-09-13 17:47:27 +0000

    dept layout

---
+++ app/about/ehassa/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Footer from '@/app/components/Footer'
+
+const Ehassa = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Ehassa</div>
+      <Footer />
+    </>
+  )
+}
+
+export default Ehassa
